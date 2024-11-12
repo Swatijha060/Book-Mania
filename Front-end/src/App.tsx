f9b3471eaad747d508d8e56fa20e6ff2e5cc5d9b
Arko298 <choudhurysayantan03@gmail.com>
2024-11-12 19:03:06 +0530

    SideBar DONE.

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,15 +1,34 @@
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
+import SideBar from "./components/SideBar";
+import About from "./pages/About/about";
+import Home from "./pages/Home/home";
+import {   Route, Routes } from "react-router-dom";
+import MyLibrary from "./pages/MyLibrary/MyLibrary";
+// import Login from "./pages/Login/login";
+// import Register from "./pages/Register/register";
+// import Profile from "./pages/Profile/profile";
 
 
 function App() {
-  
-
   return (
-    <>
-      <div className="min-h-screen flex flex-col"><Footer/>
+    <div className=" relative  flex-row w-fit h-screen">
+      <div className="flex flex-row">
+      <SideBar />
+     
+      <div className="w-full  bg-red-300 p-4">
+        
+    
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/mylibrary" element={<MyLibrary />} />
+      </Routes>
+    
       </div>
-    </>
-  )
+      </div>
+      <Footer />
+      </div>
+  );
+    
 }
-
-export default App
+export default App;
--- Front-end/src/components/SideBar.tsx
+++ Front-end/src/components/SideBar.tsx
@@ -5,19 +5,22 @@ import { Link } from 'react-router-dom'
 const SideBar:  React.FC = () => {
 
   return (
-    <div className="bg-black text-white w-1/5 min-w-[200px] p-6 flex flex-col">
+    <div className="bg-black text-white w-1/5 min-w-[200px] min-h-screen sticky p-6 flex flex-col">
       <h1 className="text-3xl font-bold mb-10">BookMania</h1>
       <nav>
-        <Link to="/home" className="flex items-center mb-6 text-lg hover:text-gray-400">
+        <Link to="/home" className="flex items-center mb-6 text-lg hover:text-[#A85C5C]">
           <FaHome className="mr-4" /> Home
         </Link>
-        <Link to="/about" className="flex items-center mb-6 text-lg hover:text-gray-400">
+        <Link to="/about" className="flex items-center mb-6 text-lg hover:text-[#A85C5C]">
           <FaInfoCircle className="mr-4" /> About Us
         </Link>
-        <Link to="/genre" className="flex items-center mb-6 text-lg hover:text-gray-400">
+        <Link to="/genre" className="flex items-center mb-6 text-lg hover:text-[#A85C5C]">
           <FaBook className="mr-4" /> Genre
         </Link>
-        <Link to="/settings" className="flex items-center text-lg hover:text-gray-400">
+        <Link to="/mylibrary" className="flex items-center mb-6 text-lg hover:text-[#A85C5C]">
+          <FaBook className="mr-4" /> My Library
+        </Link>
+        <Link to="/settings" className="flex items-center text-lg hover:text-[#A85C5C]">
           <FaCog className="mr-4" /> Settings
         </Link>
       </nav>
